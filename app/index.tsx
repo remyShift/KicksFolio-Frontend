b492bc39c96e40ac2cf096fede84336d09d67005
Remy Cassagne <remy.engassac@icloud.com>
2024-12-19 22:08:34 +0100

    'done'

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,11 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold">Le début d'une aventure.</Text>
+      <Text className="text-lg">
+        Le développement de KicksFolio peut commencer. 🎉
+      </Text>
     </View>
   );
 }
