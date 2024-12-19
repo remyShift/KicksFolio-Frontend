8454574f36790da15d7624005beb099ed76fdda5
Remy Cassagne <remy.engassac@icloud.com>
2024-12-19 14:03:52 +0100

    init project

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./app/**/*.{js,jsx,ts,tsx}",
+        "./components/**/*.{js,jsx,ts,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+export default config;
