6fcd15db3290df9d183eab08b0a504a594869f0f
Remy Cassagne <remy.engassac@icloud.com>
2024-12-20 21:55:55 +0100

    add custom font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,18 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { useFonts } from 'expo-font';
 
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    'Actonia': require('../assets/fonts/Actonia.ttf'),
+    'Spacemono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)"/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F27329',
+      },
+      fontFamily: {
+        'actonia': ['Actonia', 'sans-serif'],
+        'spacemono': ['Spacemono', 'monospace'],
+        'syne': ['Syne', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
