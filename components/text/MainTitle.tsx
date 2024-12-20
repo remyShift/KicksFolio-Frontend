e6f360a1c60df0825bca4092093284d21e17c673
Remy Cassagne <remy.engassac@icloud.com>
2024-12-20 23:58:00 +0100

    add readme file

--- components/text/MainTitle.tsx
+++
@@ -1,8 +0,0 @@
-import { Text } from 'react-native';
-
-export default function MainTitle({ content }: { content: string }) {
-    return (
-        <Text className="font-actonia text-4xl text-primary">{content}</Text>
-    );
-}
-
