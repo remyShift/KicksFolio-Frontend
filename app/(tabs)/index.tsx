9798e6f314d3a30cb84fa3559e77054a177f5a17
Remy Cassagne <remy.engassac@icloud.com>
2024-12-20 23:31:18 +0100

    brand title done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
 import { View, StyleSheet } from 'react-native';
 import MainTitle from '@/components/text/MainTitle';
 import SeparatorTitle from '@/components/text/SeparatorTitle';
+import BrandTitle from '@/components/text/BrandTitle';
 
 export default function Tab() {
   return (
-    <View className="flex-1 items-center pt-16">
+    <View className="flex-1 items-center pt-16 gap-10">
       <MainTitle content="KicksFolio" />
       <SeparatorTitle content="MY COLLECTION" />
+      <SeparatorTitle content="Add some friends" textCenter={true} />
+      <BrandTitle content="New Balance" />
     </View>
   );
 }
--- components/text/SeparatorTitle.tsx
+++ components/text/SeparatorTitle.tsx
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native';
 
-export default function SeparatorTitle({ content }: { content: string }) {
+export default function SeparatorTitle({ content, textCenter = false }: { content: string, textCenter?: boolean }) {
+    const textCenterClass = textCenter ? "text-center" : "";
     return (
-        <View className="w-full flex justify-center overflow-hidden p-4">
+        <View className="w-full flex justify-center overflow-hidden px-6">
             <Text className="font-syne-extrabold w-[200%] text-4xl text-primary opacity-10 absolute">
                 {content.toUpperCase()}
             </Text>
-            <Text className="font-syne-extrabold text-lg">
+            <Text className={`font-syne-extrabold text-lg ${textCenterClass}`}>
                 {content}
             </Text>
         </View>
