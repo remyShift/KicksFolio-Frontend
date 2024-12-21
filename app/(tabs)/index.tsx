e68daf05176cb812901d5f5ba3c20516de1071d8
Remy Cassagne <remy.engassac@icloud.com>
2024-12-21 16:17:08 +0100

    add separot title

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native';
 import MainTitle from '@/components/text/MainTitle';
+import SeparatorTitle from '@/components/text/SeparatorTitle';
 
 export default function Tab() {
   return (
     <View className="flex-1 items-center pt-20">
       <MainTitle>KicksFolio</MainTitle>
+      <SeparatorTitle content="My collection" />
+      <SeparatorTitle content="Add some friends" isTextCenter={true} />
     </View>
   );
 }
---
+++ components/text/SeparatorTitle.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+
+export default function SeparatorTitle({ content, isTextCenter = false }: { content: string, isTextCenter?: boolean }) {
+    return (
+        <View className="w-full flex justify-center overflow-hidden px-6">
+            <Text className="font-syne-extrabold w-[200%] text-4xl text-primary opacity-10 absolute">
+                {content.toUpperCase()}
+            </Text>
+            <Text className={`font-syne-extrabold text-lg ${isTextCenter ? 'text-center' : ''}`}>
+                {content}
+            </Text>
+        </View>
+    );
+}
