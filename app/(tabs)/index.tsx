290a5f48368e41ccee6401c89b0ae0d28eb090cf
Remy Cassagne <remy.engassac@icloud.com>
2024-12-22 16:20:46 +0100

    brand title done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,17 @@
 import { View, Text, StyleSheet } from 'react-native';
 import MainTitle from '@/components/text/MainTitle';
-import SeparatorTitle from '@/components/text/SeparatorTitle';
+import Title from '@/components/text/Title';
+import BrandTitle from '@/components/text/BrandTitle';
 
 export default function Tab() {
-  return (
-    <View className="flex-1 items-center pt-20">
-      <MainTitle>KicksFolio</MainTitle>
-      <SeparatorTitle content="My collection" />
-      <SeparatorTitle content="Add some friends" isTextCenter={true} />
-    </View>
-  );
+    return (
+        <View className="flex-1 items-center pt-20 gap-10">
+            <MainTitle>KicksFolio</MainTitle>
+            <Title content="My collection" />
+            <Title content="Add some friends" isTextCenter={true} />
+            <BrandTitle content="New Balance" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
--- components/text/Title.tsx
+++ components/text/Title.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native';
 
-export default function SeparatorTitle({ content, isTextCenter = false }: { content: string, isTextCenter?: boolean }) {
+export default function Title({ content, isTextCenter = false }: { content: string, isTextCenter?: boolean }) {
     return (
         <View className="w-full flex justify-center overflow-hidden px-6">
-            <Text className="font-syne-extrabold w-[200%] text-4xl text-primary opacity-10 absolute">
+            <Text className="font-syne-extrabold w-[200%] text-4xl text-primary opacity-15 absolute">
                 {content.toUpperCase()}
             </Text>
             <Text className={`font-syne-extrabold text-lg ${isTextCenter ? 'text-center' : ''}`}>
