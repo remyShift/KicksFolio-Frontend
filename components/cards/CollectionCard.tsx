27a20ec9800156e01a5bae162615ac4b22fb30ba
Remy Cassagne <remy.engassac@icloud.com>
2024-12-23 09:48:56 +0100

    add collection card

--- components/cards/CollectionCard.tsx
+++ components/cards/CollectionCard.tsx
@@ -1,12 +1,24 @@
-import { View, Text, Image } from 'react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { Image, Text, View } from 'react-native';
 
 export default function SneakerCard() {
     return (
-        <View className="flex-1 bg-white rounded-md p-2 w-full h-fit gap-2 shadow-card">
-            <Image source={require('@/assets/images/sneaker1.jpg')} className="w-full h-48 rounded-md" />
+        <View className="flex-1 bg-white rounded-md p-4 w-full h-fit gap-2 shadow-card">
+            <View className="flex flex-row items-center gap-2 w-full h-24">
+                <Image source={require('@/assets/images/sneaker1.jpg')} className="w-1/2 h-full rounded-md" />
+                <Image source={require('@/assets/images/sneaker1.jpg')} className="w-1/2 h-full rounded-md" />
+            </View>
+            <View className="flex flex-row items-center gap-2 w-full h-24">
+                <View className="w-1/2 h-full bg-slate-200 rounded-md flex flex-row items-center justify-center">
+                    <MaterialCommunityIcons name="shoe-sneaker" size={24} color="white" />
+                </View>
+                <View className="w-1/2 h-full bg-slate-200 rounded-md flex flex-row items-center justify-center">
+                    <MaterialCommunityIcons name="shoe-sneaker" size={24} color="white" />
+                </View>
+            </View>
             <View className="flex flex-row justify-between items-center">
-                <Text className="font-spacemono-bold text-lg">Asics Gel Mai x Patta</Text>
-                <Text className="text-primary font-spacemono-bold text-lg">10US</Text>
+                <Text className="font-spacemono-bold text-lg">My awesome sneaker</Text>
+                <Text className="text-primary font-spacemono-bold text-lg">2 shoes</Text>
             </View>
         </View>
     );
