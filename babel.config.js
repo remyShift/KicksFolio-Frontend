f6ede236ebbf495c47433934ff6d293dcc23bb2d
Remy Cassagne <remy.engassac@icloud.com>
2025-01-04 00:46:37 +0100

    'done'

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,11 @@ module.exports = function (api) {
     api.cache(true);
     return {
       presets: [
-        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-        "nativewind/babel",
+        "babel-preset-expo",
+        "nativewind/babel"
+      ],
+      plugins: [
+        "react-native-reanimated/plugin"
       ]
     };
-  };
+};
